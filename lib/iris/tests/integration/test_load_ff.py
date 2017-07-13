b488b5345211c40c2d57b4fa151791f5647d1cd3
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2017-07-13 16:15:16 +0100

    Tidied test.

--- lib/iris/tests/integration/test_load_ff.py
+++ lib/iris/tests/integration/test_load_ff.py
@@ -30,29 +30,16 @@ class TestFF(tests.IrisTest):
         self.assertEqual(cube.shape, shape)
         self.assertArrayShapeStats(cube, shape, 281.84446, 20.13238)
 
-#        cube = cubes[2]
-#        self.assertEqual(cube.name(), 'soil_temperature')
-#        self.assertEqual(cube.shape, shape)
-#        # This one has a problem ?
-#        self.assertArrayShapeStats(cube, shape, -708933044., 508551961.)
+        cube = cubes[2]
+        self.assertEqual(cube.name(), 'soil_temperature')
+        self.assertEqual(cube.shape, shape)
+        # This one has a problem ?
+        self.assertArrayShapeStats(cube, shape, 269.7401, 31.0357)
 
         cube = cubes[3]
         self.assertEqual(cube.name(), 'surface_altitude')
         self.assertEqual(cube.shape, shape)
         self.assertArrayShapeStats(cube, shape, 377.9390, 827.8499)
-
-
-
-    @tests.skip_data
-    def test_soilcube_fails(self):
-        file_path = tests.get_data_path(('FF', 'n48_multi_field'))
-        cubes = iris.load(file_path)
-        cube = cubes[2]
-        shape = (73, 96)
-        self.assertEqual(cube.name(), 'soil_temperature')
-        self.assertEqual(cube.shape, shape)
-        # This one has a problem ?
-        self.assertArrayShapeStats(cube, shape, 269.7401, 31.0357)
 
 
 if __name__ == '__main__':
