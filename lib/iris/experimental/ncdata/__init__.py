40d582b23a79e1bced420e7712d40b2f3d2fbf85
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2022-12-07 18:41:57 +0000

    Small tidy.

--- lib/iris/experimental/ncdata/__init__.py
+++ lib/iris/experimental/ncdata/__init__.py
@@ -12,31 +12,6 @@ TODO:
   * add "direct" netcdf interfacing, i.e. to_nc4/from_nc4
 
 """
-import iris
-from iris.cube import CubeList
-import iris.fileformats.netcdf as ifn
+from ._core import NcAttribute, NcData, NcDimension, NcVariable
 
-from .dataset_like import Nc4DatasetLike
-from .xarray import from_xarray, to_xarray
-
-#
-# The primary conversion interfaces
-#
-
-
-def cubes_from_xarray(xrds: "xarray.Dataset", **xr_load_kwargs):  # noqa
-    ncdata = from_xarray(xrds, **xr_load_kwargs)
-    dslike = Nc4DatasetLike(ncdata)
-    cubes = CubeList(ifn.load_cubes(dslike))
-    return cubes
-
-
-def cubes_to_xarray(cubes, iris_save_kwargs=None, xr_save_kwargs=None):
-    iris_save_kwargs = iris_save_kwargs or {}
-    xr_save_kwargs = xr_save_kwargs or {}
-    nc4like = Nc4DatasetLike()
-    iris.save(
-        cubes, nc4like, saver=iris.fileformats.netcdf.save, **iris_save_kwargs
-    )
-    xrds = to_xarray(**xr_save_kwargs)
-    return xrds
+__all__ = ["NcAttribute", "NcData", "NcDimension", "NcVariable"]
--- lib/iris/experimental/ncdata/_core.py
+++ lib/iris/experimental/ncdata/_core.py
@@ -11,11 +11,9 @@ This is also provided with a read/write conversion interface to Xarray.
 TODO: add direct netcdf file interface (easy, but not yet).
 
 """
-from pathlib import Path
-from typing import AnyStr, Dict, Optional, Tuple, Union
+from typing import Dict, Optional, Tuple
 
 import numpy as np
-import xarray as xr
 
 #
 # A totally basic and naive representation of netCDF data.
@@ -95,113 +93,3 @@ class NcAttribute:
             if isinstance(result, bytes):
                 result = result.decode()
         return result
-
-
-class NcDataset(NcData):
-    # An interface class providing an NcData which can be converted to/from an
-    # xr.Dataset.  This is basically done by adding a small API enabling it to function
-    # as an Xarray "AbstractDataStore".
-    # This implies some embedded knowledge of Xarray, but it is very small.
-    #
-    # This code pinched from @TomekTrzeciak
-    # see https://gist.github.com/TomekTrzeciak/b00ff6c9dc301ed6f684990e400d1435
-
-    def load(self):
-        variables = {}
-        for k, v in self.variables.items():
-            attrs = {
-                name: attr._as_python_value()
-                for name, attr in v.attributes.items()
-            }
-            xr_var = xr.Variable(
-                v.dimensions, v.data, attrs, getattr(v, "encoding", {})
-            )
-            # TODO: ?possibly? need to apply usual Xarray "encodings" to convert raw
-            #  cf-encoded data into 'normal', interpreted xr.Variables.
-            xr_var = xr.conventions.decode_cf_variable(k, xr_var)
-            variables[k] = xr_var
-        attributes = {
-            name: attr._as_python_value()
-            for name, attr in self.attributes.items()
-        }
-        return variables, attributes
-
-    def store(
-        self,
-        variables,
-        attributes,
-        check_encoding_set=frozenset(),
-        writer=None,
-        unlimited_dims=None,
-    ):
-        for attrname, v in attributes.items():
-            if attrname in self.attributes:  # and self.attributes[k] != v:
-                msg = (
-                    f're-setting of attribute "{attrname}" : '
-                    f"was={self.attributes[attrname]}, now={v}"
-                )
-                raise ValueError(msg)
-            else:
-                self.attributes[attrname] = NcAttribute(attrname, v)
-
-        for varname, var in variables.items():
-            if varname in self.variables:
-                raise ValueError(f'duplicate variable : "{varname}"')
-
-            # An xr.Variable : remove all the possible Xarray encodings
-            # These are all the ones potentially used by
-            # :func:`xr.conventions.decode_cf_variable`, in the order in which they
-            # would be applied.
-            var = xr.conventions.encode_cf_variable(
-                var, name=varname, needs_copy=False
-            )
-
-            for dim_name, size in zip(var.dims, var.shape):
-                if dim_name in self.dimensions:
-                    if self.dimensions[dim_name].size != size:
-                        raise ValueError(
-                            f"size mismatch for dimension {dim_name!r}: "
-                            f"{self.dimensions[dim_name]} != {size}"
-                        )
-                else:
-                    self.dimensions[dim_name] = NcDimension(
-                        dim_name, size=size
-                    )
-
-            attrs = {
-                name: NcAttribute(name, value)
-                for name, value in var.attrs.items()
-            }
-            nc_var = NcVariable(
-                name=varname,
-                dimensions=var.dims,
-                attributes=attrs,
-                data=var.data,
-                group=self,
-            )
-            self.variables[varname] = nc_var
-
-    def close(self):
-        pass
-
-    #
-    # This interface supports conversion to+from an xarray "Dataset".
-    # N.B. using the "AbstractDataStore" interface preserves variable contents, being
-    # either real or lazy arrays.
-    #
-    @classmethod
-    def from_xarray(
-        cls, dataset_or_file: Union[xr.Dataset, AnyStr, Path], **xr_load_kwargs
-    ):
-        if not isinstance(dataset_or_file, xr.Dataset):
-            # It's a "file" (or pathstring, or Path ?).
-            dataset_or_file = xr.load_dataset(
-                dataset_or_file, **xr_load_kwargs
-            )
-        nc_data = cls()
-        dataset_or_file.dump_to_store(nc_data, **xr_load_kwargs)
-        return nc_data
-
-    def to_xarray(self, **xr_save_kwargs) -> xr.Dataset:
-        ds = xr.Dataset.load_store(self, **xr_save_kwargs)
-        return ds
